cc28b1cc6491e0fbbe013866b013517c7d26bd54
Isaac Lim <kaishaeng@gmail.com>
2026-06-04 16:00:55 +0800

    Update resume

--- my-app/components/aboutMe.js
+++ my-app/components/aboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
 
     return (
         <div className={`${darkMode ? "text-white" : "text-black"} flex flex-row justify-start text-sm mb-4`}>
-            I am currently a Penultimate Computer Engineering Student @ NUS looking for an internship between 11 May - 1 Aug 2026 (summer).<br/>
+            I am currently a Penultimate Computer Engineering Student @ NUS looking for a 6-month part-time internship starting Jan 2027.<br/>
             <br/>
             I have a strong interest in AI Engineering, particularly in building scalable and impactful systems.
             My passion lies in integrating intelligent solutions into real-world applications, and I am keen to further strengthen my software engineering skills.
